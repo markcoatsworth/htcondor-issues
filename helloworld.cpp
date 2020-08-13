48eeea31a8f852d237082c4f402824673f8af634
Mark Coatsworth <coatsworth@cs.wisc.edu>
2020-08-13 10:19:43 -0500

    Another test commit message (#2)

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main() {
 	cout << "Hello, world!" << endl;
 	cout << "Attempting to link this commit to an issue..." << endl;
+	cout << "Another code change which I'll link to #2 in the commit message..." << endl;
 	return 0;
 }
